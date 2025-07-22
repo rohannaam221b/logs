17e3808bdff6f7f7d9222351aebd9bc4a3b6dbd5
rohan rao <rohanrao@ACCTHNMAC0138-ROHAN-R.local>
2025-07-22 16:19:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import APILogsDashboard from './APILogsDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <APILogsDashboard />
+    </div>
+  );
+}
+
+export default App;
